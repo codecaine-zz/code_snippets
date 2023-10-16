dae993373c99db64cf81599e8bf76fd218d945fd
codecaine <codecaine@gmail.com>
2023-10-16 13:52:21 -0500

    added webview for index.html with style adjustment

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from code_snippet_manager import CodeSnippetManager
-from pprint import pprint
+import webview
+
+# load index.html
 
 
+def load_html(window):
+    with open('index.html', 'r') as f:
+        html = f.read()
+        window.load_html(html)
+
+    with open('css/w3.css', 'r') as f:
+        css = f.read()
+
+    window.load_css(css)
 
 
+if __name__ == '__main__':
+    window = webview.create_window('Code Snippet Manager', width=1000, height=800, resizable=True)
+    webview.start(load_html, window)
